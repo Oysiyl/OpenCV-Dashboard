23c4cfc267503750c2d8bdb2c74c007b7cae6b03
Dmitriy Kisil <logart1995@gmail.com>
2019-02-08 19:39:07 +0200

    Added variable server in try_dash.py

--- try_dash.py
+++ try_dash.py
@@ -13,6 +13,9 @@ satellite = Orbital('TERRA')
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
+
+server = app.server
+
 app.layout = html.Div(
     html.Div([
         html.H4('TERRA Satellite Live Feed'),
